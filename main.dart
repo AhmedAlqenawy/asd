e4ec75837a0a29ab030162dac390e0b7c74954b4
AhmedAlqenawy <57736654+AhmedAlqenawy@users.noreply.github.com>
2020-02-03 00:54:53 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:untitled2/Home.dart';
+import 'package:sqflite/sqflite.dart';
+import 'Registration.dart';
+
+void main() => runApp(new MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Home(),
+  title: "New app",
+));
